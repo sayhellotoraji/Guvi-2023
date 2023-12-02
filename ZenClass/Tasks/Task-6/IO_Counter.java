748a15db30c4339b9d03a938d927cff2cb9ab82d
Rajasekar T <sayhellotoraji01@gmail.com>
2023-12-02 19:42:20 +0530

    Task 6 on IO

--- ZenClass/Tasks/Task-6/IO_Counter.java
+++ ZenClass/Tasks/Task-6/IO_Counter.java
@@ -4,34 +4,34 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class IO_Counter {
-    public static void main(String[] args) throws IOException {
-        String file_1 = args[0];
 
-        FileReader reader = new FileReader(file_1);
-        BufferedReader br = new BufferedReader(reader);
-        
-        int character_count = 0;
-        int word_count = 0;
-        int line_count = 0;
-        
-        String currentLine = br.readLine();
-        while(currentLine!=null){
-            line_count++;
+  public static void main(String[] args) throws IOException {
+    String file_1 = args[0];
 
-            String[] words = currentLine.split(" ");
-            word_count+=words.length;
+    // IO_Counter_Input_File_1.txt
+    FileReader reader = new FileReader(file_1);
+    BufferedReader br = new BufferedReader(reader);
 
-            for (String word : words) {
-                character_count+=word.length();
-            }
+    int character_count = 0;
+    int word_count = 0;
+    int line_count = 0;
 
-            currentLine = br.readLine();
-        }
+    String currentLine = br.readLine();
+    while (currentLine != null) {
+      line_count++;
 
-        System.out.println("Number of characters: "+character_count);
-        System.out.println("Number of words: "+word_count);
-        System.out.println("Number of lines: "+line_count);
+      String[] words = currentLine.split(" ");
+      word_count += words.length;
 
+      for (String word : words) {
+        character_count += word.length();
+      }
+
+      currentLine = br.readLine();
     }
-    
+
+    System.out.println("Number of characters: " + character_count);
+    System.out.println("Number of words: " + word_count);
+    System.out.println("Number of lines: " + line_count);
+  }
 }
---
+++ ZenClass/Tasks/Task-6/IO_Diff_Checker.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class IO_Diff_Checker {
+
+  public static void main(String[] args) throws IOException {
+    String file_1 = args[0];
+    String file_2 = args[1];
+
+    // IO_Diff_Checker_File_1.txt
+    FileReader fr_1 = new FileReader(file_1);
+    BufferedReader reader_1 = new BufferedReader(fr_1);
+
+    // IO_Diff_Checker_File_2.txt
+    FileReader fr_2 = new FileReader(file_2);
+    BufferedReader reader_2 = new BufferedReader(fr_2);
+
+    String currentLine_1 = reader_1.readLine();
+    String currentLine_2 = reader_2.readLine();
+    int line = 0;
+    System.out.println("Differences between 2 files: ");
+    while (currentLine_1 != null && currentLine_2 != null) {
+      line++;
+      if (!currentLine_1.equals(currentLine_2)) {
+        if (currentLine_1.length() == 0 && currentLine_2.length() > 0) {
+          System.out.println("File 2 >>> " + currentLine_2 + " Line: " + line);
+        } else if (currentLine_2.length() == 0 && currentLine_1.length() > 0) {
+          System.out.println("File 1 >>> " + currentLine_1 + " Line: " + line);
+        } else {
+          System.out.println("File 1 >>> " + currentLine_1 + " Line: " + line);
+          System.out.println("File 2 >>> " + currentLine_2 + " Line: " + line);
+        }
+      }
+      System.out.println();
+
+      currentLine_1 = reader_1.readLine();
+      currentLine_2 = reader_2.readLine();
+    }
+  }
+}
