d0b81045e8c945561edb37e2a426b00af5340ecf
Rajasekar T <sayhellotoraji01@gmail.com>
2023-12-02 18:56:35 +0530

    Task 6

---
+++ ZenClass/Tasks/Task-6/IO_Counter.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class IO_Counter {
+    public static void main(String[] args) throws IOException {
+        String file_1 = args[0];
+
+        FileReader reader = new FileReader(file_1);
+        BufferedReader br = new BufferedReader(reader);
+        
+        int character_count = 0;
+        int word_count = 0;
+        int line_count = 0;
+        
+        String currentLine = br.readLine();
+        while(currentLine!=null){
+            line_count++;
+
+            String[] words = currentLine.split(" ");
+            word_count+=words.length;
+
+            for (String word : words) {
+                character_count+=word.length();
+            }
+
+            currentLine = br.readLine();
+        }
+
+        System.out.println("Number of characters: "+character_count);
+        System.out.println("Number of words: "+word_count);
+        System.out.println("Number of lines: "+line_count);
+
+    }
+    
+}
