5c86e99e09d87f454ed01a8693387b989a2aa134
Rajasekar T <sayhellotoraji01@gmail.com>
2024-01-06 14:51:48 +0530

    Junit demo setup using maven

---
+++ Practice Development Modules/spring_projects/jUnit_test/src/test/java/com/java/junit/JUnitTestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.java.junit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.java.junit.module.Calculator;
+
+@SpringBootTest
+class JUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
---
+++ Practice Development Modules/spring_projects/jUnit_test/src/test/java/com/java/junit/testcases/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.java.junit.testcases;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// 1. Don' t use this annotation for Test 
+// --->>cause individual test class gets terminated
+//import org.junit.Test;
+
+// Instead use this annotation
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.java.junit.module.Calculator;
+
+public class CalculatorTest {
+
+	// 2. Static or non static methods for different annotations
+	// Add @BeforeAll annotation to Static method
+
+	// All the @Test annotation can be added for non-static test methods
+	@Test
+	public void CalcTest() {
+		System.out.println(Calculator.add(12, 12));
+		assertEquals(24, Calculator.add(12, 12));
+	}
+
+	// Add @AfterAll annotation to Static method
+}
