688886b81c127c9f5ee5add3c0665374e34d9b20
Rajasekar T <sayhellotoraji01@gmail.com>
2024-01-10 20:56:46 +0530

    JUnit Worked

--- Practice Development Modules/spring_projects/spring_jUnit/src/main/java/com/java/junit/module/Calculator.java
+++ Practice Development Modules/spring_projects/spring_jUnit/src/main/java/com/java/junit/module/Calculator.java
@@ -1,7 +1,21 @@
 package com.java.junit.module;
 
 public class Calculator {
-	public static int add(int num1, int num2) {
-		return num1 + num2;
+
+	CalculatorService calcService;
+
+	public Calculator(CalculatorService calculatorService) {
+		calcService = calculatorService;
 	}
+
+	public int perform(int num1, int num2) {
+//		return add(num1, num2);
+		return calcService.add(num1, num2);
+
+	}
+	
+	public int add(int num1, int num2) {
+		return num1+num2;
+	}
+
 }
