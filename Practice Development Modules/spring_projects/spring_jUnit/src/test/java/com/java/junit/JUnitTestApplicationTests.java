ce52062d84e54fd05d64dfc1a35735e1a4292c64
Rajasekar T <sayhellotoraji01@gmail.com>
2024-01-11 13:55:36 +0530

    JUnit Mockito working notes add in main parent Test tile

--- Practice Development Modules/spring_projects/spring_jUnit/src/test/java/com/java/junit/JUnitTestApplicationTests.java
+++ Practice Development Modules/spring_projects/spring_jUnit/src/test/java/com/java/junit/JUnitTestApplicationTests.java
@@ -16,3 +16,46 @@ class JUnitTestApplicationTests {
 	}
 
 }
+
+/*
+	3 Ways to enable Mockito annotation
+
+1. @ExtendWith(MockitoExtension.class) 
+Add to the test class - not working, 
+Add mockito-junit-jupiter dependencies - working
+
+2. When using @Mock & @InjectMocks
+Add MockitoAnnotations.openMocks(this) 
+in a setup method with @BeforeEach annotation - working
+
+3. Check later
+
+		
+			Creating Mocks
+
+1. Using @Mock & @InjectMocks
+2. Using mock() method
+
+@BeforeEach
+public void setUp() {
+empRepo = Mockito.mock(EmployeeRepository.class);
+empService = new EmployeeService(empRepo);
+}	
+
+
+
+Important things to be followed
+
+1. @ExtendWith, @Mock, @InjectMocks, openMocks - Used together
+2. Test methods need to be public, void - Reflection might be used
+3. Another way to mock with out using @Mock and @InjectMocks
+
+
+
+	Static or non static methods for different annotations
+
+1. @BeforeAll annotation to Static method 
+2. @AfterAll annotation to Static method 
+3. @BeforeEach, @Test, @AfterEach annotation to Non-static test method
+
+*/
--- Practice Development Modules/spring_projects/spring_jUnit/src/test/java/com/java/junit/testcases/CalculatorTest.java
+++ Practice Development Modules/spring_projects/spring_jUnit/src/test/java/com/java/junit/testcases/CalculatorTest.java
@@ -1,55 +1,18 @@
 package com.java.junit.testcases;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
-
-// 1. Don' t use this annotation for Test 
-// --->>cause individual test class gets terminated
-//import org.junit.Test;
-
-// Instead use this annotation
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;// Use for Test
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.java.junit.module.Calculator;
 
-/*
- * 			3 Ways to enable Mockito annotation
- *  
- *  1. @ExtendWith(MockitoExtension.class) 
- *  	Add to the test class - not working
- *  
- *  2. Add MockitoAnnotations.openMocks(this) in a setup method 
- *  	with @BeforeEach annotation - working
- *  
- *  3. Check later
- */
-/**********************************************************************/
-/*
- * 			Static or non static methods for different annotations
- * 
- * 1. @BeforeAll annotation to Static method 2. @AfterAll annotation to Static
- * method 3. @Test annotation to Non-static test method
- */
-
-/*********************************************************************/
-
-/*
- * openMocks is mandatory to add if @Mock annotation is used
- * 
- * @BeforeEach void init() { MockitoAnnotations.openMocks(this); }
- * 
- * test methods always need to be public, void - because Reflection is used
- */
 
 public class CalculatorTest {
-	final int var1 = 10;
-	final int var2 = 20;
 
 	// Mock Creation - Method 1
 	// Calculator cal = mock(Calculator.class);
@@ -57,14 +20,13 @@ public class CalculatorTest {
 	// Mock Creation - Method 2
 	@Mock
 	Calculator cal;
-	
+
 	@BeforeEach
 	public void setup() {
 		MockitoAnnotations.openMocks(this);
 	}
-	
-	/******************************************************************/
-	
+
+
 	// Invocation - Remember that each call to a method within Mockito methods
 	@Test
 	public void CalcTest() {
@@ -72,8 +34,5 @@ public class CalculatorTest {
 		when(cal.perform(10, 10)).thenReturn(25);
 		verify(cal).perform(10, 10);
 	}
-	
-	
-	
 
 }
